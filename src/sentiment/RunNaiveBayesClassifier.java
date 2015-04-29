6d41b9d3cb514a8cb13a3d7bd73bb482c732104f
aziz22q <aziz22q@mtholyoke.edu>
2015-04-28 21:16:08 -0400

    new data files and changes to sentiment

--- src/sentiment/RunNaiveBayesClassifier.java
+++ src/sentiment/RunNaiveBayesClassifier.java
@@ -3,75 +3,56 @@ package sentiment;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.text.DecimalFormat;
 import java.util.Arrays;
-import java.util.Set;
 
 import de.daslaboratorium.machinelearning.classifier.*;
 
 /**
  * 
- * @author Qudsia & Grace
+ * @author Qudsia
  *
  */
 public class RunNaiveBayesClassifier {
 	
-	private static final String INPUT_FILE_NAME = "reviews_2000.txt";
-	private static String predicted="";
+	//private static final String INPUT_FILE_NAME = "reviews_5000.txt";
+	//private static final String INPUT_FILE_NAME = "movie.reviews";
+	private static final String INPUT_FILE_NAME = "movie.reviews.tagged";
+	//private static final String INPUT_FILE_NAME = "reviews_2000_negated.txt";
 	
-	private static double numReviews = 0.0;
-	
-	private static double truePos = 0.0; // count the correct predictions
-	private static double falsePos = 0.0;
-	private static double trueNeg = 0.0;
-	private static double falseNeg =0.0;
+	public final static Classifier<String, String> bayes = new BayesClassifier<String, String>();
 	
 	public static void main(String[] args) throws IOException {
 
 		RunNaiveBayesClassifier.readReviewsFile();
-		
-		double tPPercent = truePos/numReviews;
-		double fPPercent = falsePos/numReviews;
-		double tNPercent = trueNeg/numReviews;
-		double fNPercent = falseNeg/numReviews;
-		
-		double correctPercent = (truePos+trueNeg)/numReviews;
-		double incorrectPercent = (falsePos+falseNeg)/numReviews;
-		
-		
-		System.out.println("doubleval "+correctPercent);
-		
-		System.out.println("\ntruePos: "+truePos+" falsePos: "+falsePos+"\ntrueNeg: "
-		+trueNeg+" falseNeg: "+falseNeg+"\ntotal correct: "+correctPercent+" incorrect: "+incorrectPercent);
 	}
 
 	/**
 	 * 
 	 * @throws IOException
 	 */
-	private static void readReviewsFile() throws IOException{
+	public static void readReviewsFile() throws IOException{
 
-		BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE_NAME));		
-		
-		final Classifier<String, String> bayes = new BayesClassifier<String, String>();
+		BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE_NAME));			
+
 		
 		String line = br.readLine();
-
+		
+		float correct=0;
 		int counter = 0;
-		int trainon = 1900;
 		
 		while (line != null) {	
 			
-			String[] tokens = line.split(" ");
-			String label = tokens[1];
+			//remove tagged info
+			String strippedTags = line.toLowerCase().replaceAll("<.*?>.*?</.*?>|<.*?/>", "");
+			String[] tokens = strippedTags.split("\\s+");
 			
-			// learn the first 1900
-			if(counter<trainon){
+			//String[] tokens = line.toLowerCase().split("\\s+");
+
+			if(counter<1500){
 				
 				//1 = positive
 				//0 = negative
-				if(label.equals("1")){
+				if(tokens[1].equals("1")){
 					
 					bayes.learn("positive", Arrays.asList(tokens));
 					
@@ -83,18 +64,18 @@ public class RunNaiveBayesClassifier {
 	        		
 			counter++;
 			
-			// test on the rest
-			if(counter>=trainon){
+			if(counter>=1500){
 				
-				System.out.println();
-				System.out.println(line);
-				// this returns a Set<K> of categories bayes knows about
-				predicted = bayes.classify(Arrays.asList(tokens)).getCategory();
-				System.out.println("predicted: "+predicted);
+//				System.out.println();
+//				System.out.println(line);
 				
-				// validate this example by passing prediction + label
-				validate(predicted, label);
+				String prediction = bayes.classify(Arrays.asList(tokens)).getCategory();
+//				System.out.println("predicted: " + prediction);
+//				System.out.println();
 				
+				if(prediction.equals("negative")&&tokens[1].equals("0") || prediction.equals("positive")&&tokens[1].equals("1")){
+					correct++;
+				}
 			}
 			
 			
@@ -103,39 +84,11 @@ public class RunNaiveBayesClassifier {
 			((BayesClassifier<String, String>) bayes).classifyDetailed(Arrays.asList(tokens));
 		}
 		
-		// the number of reviews we've tested
-		numReviews = counter-trainon;
 	    bayes.setMemoryCapacity(2000); 
+	    
+	    System.out.println("accurracy: " + (float)(correct/(float)500));
 
 		br.close();
 
-	}
-	
-	/**
-	 * 
-	 * @param guess
-	 * @param correct
-	 * @return
-	 */
-	public static void validate(String guess, String correct){
-		// a correct guess can be positive with label 1
-		if (guess.trim().matches("positive")){
-			if (correct.trim().matches("1")){
-				truePos ++;
-			}
-			else{
-				falsePos ++;
-			}
-		}
-		// a correct guess can be negative with label 0
-		else{
-			if(correct.trim().matches("0")){
-				trueNeg ++;
-			}
-			else{
-				falseNeg++;
-			}
-		}
-		
 	}
 }
